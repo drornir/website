6921d29f47cdd82719729f241764de69257134c4
dror nir <me@drornir.dev>
2024-09-06 19:42:13 +0300

    fix redirects

--- astro/my-shadcn/astro.config.mjs
+++ astro/my-shadcn/astro.config.mjs
@@ -5,33 +5,37 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import cloudflare from "@astrojs/cloudflare";
 
-const config ={
-    site: "https://drornir.dev",
+const config = {
+  site: "https://drornir.dev",
 
-    integrations: [
-      mdx(),
-      sitemap(),
-      react(),
-      tailwind({
-        applyBaseStyles: false,
-      }),
-    ],
-
-    output: "server",
-    adapter: cloudflare({
-        imageService: "cloudflare",
-        platformProxy: {
-            enabled: false,
-        }
+  integrations: [
+    mdx(),
+    sitemap(),
+    react(),
+    tailwind({
+      applyBaseStyles: false,
     }),
+  ],
 
-    redirects: {
-        "/posts/*": {
-          status: 302,
-          destination: "/blog/:splat",
-        }
-      },
-  }
+  output: "server",
+  adapter: cloudflare({
+    imageService: "cloudflare",
+    platformProxy: {
+      enabled: false,
+    },
+  }),
+
+  redirects: {
+    "/posts": {
+      status: 301,
+      destination: "/blog",
+    },
+    "/posts/*": {
+      status: 301,
+      destination: "/blog/:splat",
+    },
+  },
+};
 
 // https://astro.build/config
 export default defineConfig(config);
