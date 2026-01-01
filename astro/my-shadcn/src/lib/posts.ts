f6f1e7dbf47bbe03abe11826a78c20e0e0d4b285
dror nir <me@drornir.dev>
2026-01-01 20:11:28 +0200

    github-actions-dynamic-envs + tags page(s)

--- astro/my-shadcn/src/lib/posts.ts
+++ astro/my-shadcn/src/lib/posts.ts
@@ -1,8 +1,9 @@
 import { getCollection } from "astro:content"
 
-export async function getPosts() {
+export async function getPosts({ tag }: { tag?: string } = {}) {
   return await getCollection("blog").then((posts) =>
     posts
+      .filter(({ data }) => (tag ? data.tags.includes(tag) : true))
       .filter(({ data }) => import.meta.env.DEV || !data.draft)
       .sort((a, b) => {
         const ad = a.data.updatedDate ?? a.data.date
@@ -10,4 +11,10 @@ export async function getPosts() {
         return bd.valueOf() - ad.valueOf()
       }),
   )
+}
+
+export async function getTags() {
+  const posts = await getPosts()
+  const tags = posts.flatMap((post) => post.data.tags)
+  return [...new Set(tags)].sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase(), "en", { sensitivity: "base" }))
 }
