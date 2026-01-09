b6bcab5f760e63ef89947fde4637554c041239f6
dror nir <me@drornir.dev>
2026-01-09 17:00:16 +0200

    start the blog post, and unrelated site restructure

--- astro/my-shadcn/src/content/config.ts
+++ astro/my-shadcn/src/content/config.ts
@@ -1,21 +1,25 @@
 import { defineCollection, z } from "astro:content"
 
+export const blogPostDataZodSchema = z.object({
+  title: z.string(),
+  description: z.string().default(""),
+  date: z.coerce.date(),
+  updatedDate: z.coerce.date().optional(),
+  cover: z.object({
+    image: z.string(),
+    caption: z.string().optional(),
+    alt: z.string().optional(),
+  }),
+  tags: z.array(z.string()),
+  draft: z.boolean().optional(),
+})
+
+export type BlogPostData = z.infer<typeof blogPostDataZodSchema>
+
 const blog = defineCollection({
   type: "content",
   // Type-check frontmatter using a schema
-  schema: z.object({
-    title: z.string(),
-    description: z.string().default(""),
-    date: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-    cover: z.object({
-      image: z.string(),
-      caption: z.string().optional(),
-      alt: z.string().optional(),
-    }),
-    tags: z.array(z.string()),
-    draft: z.boolean().optional(),
-  }),
+  schema: blogPostDataZodSchema,
 })
 
 export const collections = { blog }
